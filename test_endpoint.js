dd40bc96570c8c63d6790a26d8eb05077a8f3296
Najmi Nasir <nnajmi302@gmail.com>
2026-06-24 20:27:01 +0800

    test

--- test_endpoint.js
+++
@@ -1,7 +0,0 @@
-const http = require("http");
-http.get("http://127.0.0.1:3000/leaderboard", (res) => {
-  console.log("Status:", res.statusCode);
-  let data = "";
-  res.on("data", chunk => data += chunk);
-  res.on("end", () => console.log("Body:", data.substring(0, 100)));
-}).on("error", (err) => console.log("Error:", err.message));
--- test_endpoint5005.js
+++
@@ -1,7 +0,0 @@
-const http = require("http");
-http.get("http://127.0.0.1:5005/users/me/submitted-reviews", (res) => {
-  console.log("Status:", res.statusCode);
-  let data = "";
-  res.on("data", chunk => data += chunk);
-  res.on("end", () => console.log("Body:", data.substring(0, 100)));
-}).on("error", (err) => console.log("Error:", err.message));
